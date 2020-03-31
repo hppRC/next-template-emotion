d81e977a357a52a9e4d38bd30a1f2d63bdf396b2
hppRC <hpp.ricecake@gmail.com>
2020-04-01 00:22:12 +0900

    :tada: add fundamental configs and components

---
+++ install.sh
@@ -0,0 +1,21 @@
+yarn add -D \
+    @types/react \
+    stylelint \
+    stylelint-config-recess-order \
+    stylelint-config-recommended \
+    stylelint-config-standard \
+    stylelint-config-styled-components \
+    stylelint-declaration-block-no-ignored-properties \
+    stylelint-no-unsupported-browser-features \
+    stylelint-order \
+    typescript@beta \
+    @typescript-eslint/eslint-plugin \
+    @typescript-eslint/parser \
+    eslint \
+    eslint-config-airbnb \
+    eslint-config-prettier \
+    eslint-plugin-import \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-prettier \
+    eslint-plugin-react \
+    prettier 
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable
+    @typescript-eslint/no-var-requires,
+    @typescript-eslint/explicit-function-return-type
+*/
+
+const { resolve } = require(`path`);
+
+module.exports = {
+  target: `serverless`,
+  webpack: config => {
+    config.resolve.alias[`~`] = resolve(__dirname, `src`);
+    return config;
+  }
+};
