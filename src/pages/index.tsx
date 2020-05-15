ca6dfd31dfc67b1ce4e22fbdbe3e0636e08520c1
hppRC <hpp.ricecake@gmail.com>
2020-05-16 00:29:08 +0900

    :up: Update packages versions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,10 @@ import styled from '@emotion/styled';
 type ContainerProps = {};
 type Props = {} & ContainerProps;
 
-const Component: React.FCX<Props> = ({ className }) => <div className={className}>jladkjklaj</div>;
+const Component: React.FCX<Props> = ({ className }) => <div className={className}>index page</div>;
 
-const StyledComponent = styled(Component)`
-  @media screen and (max-width: 1100px) {
-  }
-  @media screen and (max-width: 768px) {
-  }
-  @media screen and (max-width: 480px) {
-  }
-  @media screen and (max-height: 430px) {
-  }
-`;
+const StyledComponent = styled(Component)``;
 
 const Container: React.FCX<ContainerProps> = (props) => <StyledComponent {...props} />;
 
-export const config = { amp: true };
 export default Container;
