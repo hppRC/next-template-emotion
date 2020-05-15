5476e4179aa68039515ccd3b7858426090e8f9a0
hppRC <hpp.ricecake@gmail.com>
2020-05-16 01:56:35 +0900

    :+1: Add test page

---
+++ src/pages/test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+type ContainerProps = {};
+type Props = {} & ContainerProps;
+
+const Component: React.FCX<Props> = ({ className }) => <div className={className}>this is test page</div>;
+
+const StyledComponent = styled(Component)``;
+
+const Container: React.FCX<ContainerProps> = (props) => <StyledComponent {...props} />;
+
+export default Container;
