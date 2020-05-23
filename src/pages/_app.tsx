08f8e771b5a9da47853c442f6733283f58406f7a
hppRC <hpp.ricecake@gmail.com>
2020-05-24 02:52:33 +0900

    :+1: Update type definitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import 'sanitize.css';
 import { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
-export default ({ Component, pageProps }: AppProps) => (
+export default ({ Component, pageProps }: AppProps): JSX.Element => (
   <RecoilRoot>
     <Component {...pageProps} />
   </RecoilRoot>
--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -3,5 +3,5 @@
 import React from 'react';
 
 declare module 'react' {
-  type FCX<P = {}> = React.FunctionComponent<P & { className?: string }>;
+  type FCX<P = Record<string, unknown>> = React.FunctionComponent<P & { className?: string }>;
 }
